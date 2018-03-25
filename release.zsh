d1e385e1e350af51089383f39a62ac4457e782f8
Alexander Nelzin <asnelzin@gmail.com>
2018-03-25 15:49:03 +0300

    Fix release script

--- release.zsh
+++ release.zsh
@@ -7,19 +7,19 @@ if ( echo $VERSION | grep '^v' ); then
 	exit
 fi
 
-set REV $(git rev-parse --short HEAD)
+REV=$(git rev-parse --short HEAD)
 echo Tagging $REV as v$VERSION
 git tag --annotate v$VERSION -m "Release v$VERSION"
 echo Be sure to: git push --tags
 echo
 
-set DISTDIR target/v$VERSION
+DISTDIR=target/v$VERSION
 mkdir -p $DISTDIR
 
-for pair in linux/386 linux/amd64 darwin/amd64 then
-	set GOOS   $(echo $pair | cut -d'/' -f1)
-	set GOARCH $(echo $pair | cut -d'/' -f2)
-	set BIN    $DISTDIR/ws-$VERSION-$GOOS-$GOARCH
+for pair in linux/386 linux/amd64 darwin/amd64; do
+	GOOS=$(echo $pair | cut -d'/' -f1)
+	GOARCH=$(echo $pair | cut -d'/' -f2)
+	BIN=$DISTDIR/ws-$VERSION-$GOOS-$GOARCH
 	echo $BIN
 	env GOOS=$GOOS GOARCH=$GOARCH go build -o $BIN -ldflags="-X main.revision=$VERSION" ./cmd/ws
-fi
+done
