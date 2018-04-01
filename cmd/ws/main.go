cd5c95fb0d7a36a73ca317396e95c485952c218f
Alexander Nelzin <asnelzin@gmail.com>
2018-04-01 12:28:05 +0300

    Refactor, add prompt, improve cli

--- cmd/ws/main.go
+++ cmd/ws/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/chzyer/readline"
 	"github.com/fatih/color"
 	"github.com/gorilla/websocket"
 	"github.com/jessevdk/go-flags"
@@ -23,6 +24,12 @@ var opts struct {
 	Version bool     `short:"v" long:"version" description:"Print the version information and exit"`
 }
 var parser = flags.NewParser(&opts, flags.Default)
+
+var (
+	green  = color.New(color.FgGreen).SprintfFunc()
+	yellow = color.New(color.FgYellow).SprintfFunc()
+	blue   = color.New(color.FgBlue).SprintfFunc()
+)
 
 func main() {
 	parser.Usage = usage
@@ -39,32 +46,29 @@ func main() {
 
 	if len(args) != 1 {
 		errorf("the required argument URL was not provided")
-		os.Exit(2)
+		os.Exit(1)
 	}
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
 
 	u, err := url.Parse(args[0])
 	if err != nil {
 		errorf("could not parse URL: %v", err)
+		os.Exit(1)
 	}
 
-	headers := http.Header{}
-	for _, h := range opts.Headers {
-		kv := strings.Split(h, ":")
-		if len(kv) != 2 {
-			continue
-		}
-		key, val := kv[0], kv[1]
-		if len(key) == 0 || len(val) == 0 {
-			continue
-		}
-		key, val = strings.TrimSpace(key), strings.TrimSpace(val)
-		headers.Add(key, val)
-	}
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, os.Kill)
 
-	fmt.Printf("Connecting to %s\n", u.String())
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt: green(">>> "),
+	})
+	if err != nil {
+		errorf("could not create prompt: %v", err)
+		os.Exit(2)
+	}
+	defer rl.Close()
+
+	headers := parseHeaderOpts(opts.Headers)
+
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), headers)
 	if err != nil {
 		errorf("could not dial %s: %v", u.String(), err)
@@ -72,32 +76,12 @@ func main() {
 	}
 	defer c.Close()
 
-	// reading
-	done := make(chan struct{})
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				errorf("read: %v", err)
-				return
-			}
-			printReceived(message)
-		}
-	}()
+	showVersion()
+	fmt.Printf("Connected to %s\n\n", green(u.String()))
 
-	// writing
-	go func() {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			msg := scanner.Bytes()
-			err := c.WriteMessage(websocket.TextMessage, msg)
-			if err != nil {
-				errorf("write close: %v", err)
-				return
-			}
-		}
-	}()
+	done := make(chan struct{})
+	go readPump(c, rl, done)
+	go writePump(c, rl, done, interrupt)
 
 	for {
 		select {
@@ -120,22 +104,66 @@ func main() {
 	}
 }
 
-var (
-	yellow = color.New(color.FgYellow).SprintfFunc()
-	blue   = color.New(color.FgBlue).SprintfFunc()
-)
-
-func printReceived(message []byte) {
-	t := time.Now()
-	timeStr := fmt.Sprintf("%02d:%02d:%02d", t.Hour(), t.Minute(), t.Second())
-	fmt.Printf("%s %s\n", yellow(timeStr), blue(string(message)))
-
+func parseHeaderOpts(hs []string) http.Header {
+	headers := http.Header{}
+	for _, h := range hs {
+		kv := strings.Split(h, ":")
+		if len(kv) != 2 {
+			continue
+		}
+		key, val := kv[0], kv[1]
+		if len(key) == 0 || len(val) == 0 {
+			continue
+		}
+		key, val = strings.TrimSpace(key), strings.TrimSpace(val)
+		headers.Add(key, val)
+	}
+	return headers
 }
 
-func errorf(format string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, "ws: "+format+"\n", args...)
+func writePump(conn *websocket.Conn, rl *readline.Instance, done chan struct{}, interrupt chan os.Signal) {
+	for {
+		line, err := rl.Readline()
+		if err == readline.ErrInterrupt {
+			continue
+		} else if err != nil {
+			close(interrupt)
+			return
+		}
+		if len(line) == 0 {
+			continue
+		}
+		err = conn.WriteMessage(websocket.TextMessage, []byte(line))
+		if err != nil {
+			errorf("write close: %v", err)
+			close(done)
+			return
+		}
+	}
+}
+
+func readPump(conn *websocket.Conn, rl *readline.Instance, done chan struct{}) {
+	defer close(done)
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			errorf("read: %v", err)
+			return
+		}
+		printReceived(rl.Stdout(), message)
+	}
+}
+
+func printReceived(w io.Writer, message []byte) {
+	t := time.Now()
+	timeStr := fmt.Sprintf("%02d:%02d:%02d", t.Hour(), t.Minute(), t.Second())
+	fmt.Fprintf(w, "%s %s\n", yellow(timeStr), blue(string(message)))
 }
 
 func showVersion() {
 	fmt.Printf("ws version %s\n", revision)
+}
+
+func errorf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, "ws: "+format+"\n", args...)
 }
